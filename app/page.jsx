65e1921a33d6984a94fe02819ee1f232f286deb0
grejtis <zmija.kacperek@gmail.com>
2025-12-09 21:45:26 +0100

    Siemanko

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Hello Antek 123123</p>
+      <p className="p-1">Hello Antek 123123</p>
     </div>
   );
 }
