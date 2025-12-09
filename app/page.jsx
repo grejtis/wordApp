4c61676c9e868791b546e696df50191954cf2268
grejtis <zmija.kacperek@gmail.com>
2025-12-09 21:37:40 +0100

    Updates greeting message on home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Hello Antek</p>
+      <p>Hello Antek 123123</p>
     </div>
   );
 }
